bc63aba970060dfeef95f1ecb1810aad4c46c882
unknown <sudhansu67@gmail.com>
2019-07-22 23:58:14 +0530

    first commit.

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  public name:string = 'Sudhansu (Silu)';
+  public imageSrc:string = 'http://lorempixel.com/400/200/';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
